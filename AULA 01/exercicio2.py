e60c31506fffbd77ee57b8ba0f965ccab8267abc
mxrcelorrr <mapatagorda@gmail.com>
2025-09-02 22:12:57 -0300

    EXERCICIOS DA AULA 1

---
+++ AULA 01/exercicio2.py
@@ -0,0 +1,6 @@
+nome = ("Marcelo")
+idade = 21
+altura = 1.70
+homem = True
+
+print(f"Nome {nome}\nidade {idade}\naltura {altura}\nhomem {homem}")
---
+++ AULA 01/exercicio3.py
@@ -0,0 +1,10 @@
+A = 1
+B = 2
+J = 3
+
+J= A
+A= B
+B = J
+
+print('VAR A', A)
+print('VAR B', B)
