052c8163ab4efce4d07e4a3f2b8a8a14e12f9ded
mxrcelorrr <mapatagorda@gmail.com>
2025-09-02 22:46:05 -0300

    calculo

---
+++ AULA 01/calculo.py
@@ -0,0 +1,6 @@
+raio = 5
+PI = 3.14159
+
+area = PI * raio ** 2
+
+print(f'a area da cicunferência de raio {raio} é {area:.2f}')
---
+++ AULA 01/entrada_de_dados.py
@@ -0,0 +1,4 @@
+nome = input("qual é o seu nome? ")
+
+print("Ola, " + nome + "! seja bem vindo")
+print(f"Ola, {nome}! seja bem vindo")
