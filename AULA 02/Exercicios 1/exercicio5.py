a10fff44bafe0fa96abd7f79bd28865d9c358d79
mxrcelorrr <mapatagorda@gmail.com>
2025-09-09 22:34:57 -0300

    AULA 03 presencial

--- AULA 02/Exercicios 1/exercicio5.py
+++ AULA 02/Exercicios 1/exercicio5.py
@@ -1,5 +1,15 @@
-produto = 100
+"""produto = 100
 quantidade_produto=float(input("digite a quantidade desse produto: "))
 
 if quantidade_produto > 10:
-    print(produto * quantidade_produto * 0.10)
+    print(produto * quantidade_produto)
+"""
+
+produto = float(input("Digite o valor desse produto: "))
+quantidade_produto = int(input("Digite a quantidade desse produto: "))
+
+if quantidade_produto > 10:
+    produto = produto * 0.9
+
+total = produto * quantidade_produto
+print(f"Valor total: R${total:.2f}")
---
+++ AULA 02/Exercicios 1/exercicio6.py
@@ -0,0 +1,6 @@
+idade = int(input("Digite a sua idade: "))
+
+if idade > 15:
+    print("voce pode votar")
+else:
+    print("voce nao pode votar")
