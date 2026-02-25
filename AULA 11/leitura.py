8cb3214fb4ea2956cb0d4852161576457307087e
mxrcelorrr <mapatagorda@gmail.com>
2026-02-24 22:26:41 -0300

    csv read

---
+++ AULA 11/leitura.py
@@ -0,0 +1,22 @@
+import csv
+
+caminho_arquivo = "exemplo.csv"
+
+with open(caminho_arquivo, mode='r', newline='',encoding='utf-8') as arquivo_csv:
+    leitor_csv = csv.reader(arquivo_csv)
+
+    matriz = list(arquivo_csv)
+    linhas = len(matriz)
+    colunas = len(matriz[0])
+    print(colunas)
+    print(linhas)
+    
+    for i in range(0, linhas):
+        for j in range(0, colunas):
+            print(f"| {matriz[i][j]} |", end="")
+
+
+    for linha in leitor_csv:
+        coluna1, coluna2, coluna3, coluna4, coluna5 = linha
+        print(f"Coluna 1: {coluna1}, Coluna 2: {coluna2}, Coluna 3 {coluna3}, Coluna 4 {coluna4}, Coluna 5 {coluna5}")
+
